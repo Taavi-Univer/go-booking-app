df85a8942758ff83dd964f8cdf10c73a5305c62c
Taavi-Univer <96955769+sseagull032@users.noreply.github.com>
2022-03-31 19:05:57 +0300

    update

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import "fmt"
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 70
-	var remainingTickets = 70
+	var remainingTickets uint = 70
+	var bookings []string
 
 	fmt.Printf("conferenceName is %T, conferenceTickets is %T, remainingTickets is %T\n", conferenceName, conferenceTickets, remainingTickets)
 
@@ -13,12 +14,29 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v remaining\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	var userTickets int
-	// ask user for their name and how many tickets their have
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 
-	userName = "Taavi"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	// ask user for name and how many tickets
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
 
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are all our bookings: %v\n", bookings)
 }
